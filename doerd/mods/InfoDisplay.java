4519795222c405eeb2e918ab4fc93e504f184b56
Sean Richardson <seanrichardson98@gmail.com>
2018-05-27 15:34:11 -0700

    New utility Vector.java

--- doerd/mods/InfoDisplay.java
+++ doerd/mods/InfoDisplay.java
@@ -3,6 +3,7 @@ package doerd.mods;
 import org.lwjgl.input.Keyboard;
 
 import doerd.main.Category;
+import doerd.utils.Vector;
 
 public class InfoDisplay extends Module{
 
@@ -13,6 +14,8 @@ public class InfoDisplay extends Module{
 	public void customOverlay(){
         this.mc.fontRendererObj.drawString("Pitch: " + mc.thePlayer.rotationPitch, 2, 100, 16777215);
         this.mc.fontRendererObj.drawString("Yaw: " + mc.thePlayer.rotationYaw, 2, 110, 16777215);
+        Vector direction = new Vector(mc.thePlayer.rotationPitch,mc.thePlayer.rotationYaw);
+        this.mc.fontRendererObj.drawString(direction.toString(),2,120,16777215);
 	}
 
 }
---
+++ doerd/utils/Vector.java
@@ -0,0 +1,31 @@
+package doerd.utils;
+
+public class Vector {
+	double x;
+	double y;
+	double z;
+	public Vector(double X, double Y, double Z) {
+		this.x = X;
+		this.y = Y;
+		this.z = Z;
+	}
+	public Vector(double pitch, double yaw) {
+		double theta = toRads(yaw);
+		double phi = toRads(pitch);
+		this.x = Math.cos(phi)*Math.sin(theta);
+		this.y = -1*Math.sin(phi);
+		this.z = Math.cos(phi)*Math.cos(theta);
+	}
+	public double dotProduct(Vector u) {
+		return this.x*u.x+this.y*u.y+this.z*u.z;
+	}
+	public double magnitude() {
+		return Math.sqrt(this.dotProduct(this));
+	}
+	public String toString() {
+		return String.format("<%.2f,%.2f,%.2f>",x,y,z);
+	}
+	public double toRads(double theta) {
+		return theta*(Math.PI/180);
+	}
+}
