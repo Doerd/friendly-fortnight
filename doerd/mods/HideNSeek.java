3ff94660e0e2b9e07b48b1b94b929d3383a71b41
William Adriance <williamadriance@Williams-MacBook-Pro-6.local>
2018-05-31 22:44:35 -0700

    test

---
+++ doerd/mods/HideNSeek.java
@@ -0,0 +1,28 @@
+package doerd.mods;
+
+import org.lwjgl.input.Keyboard;
+
+import doerd.main.Category;
+import net.minecraft.entity.EntityLivingBase;
+
+public class HideNSeek extends Module{
+
+	public HideNSeek() {
+		super("Hide-N-Seek", Keyboard.KEY_I, Category.RENDER);
+
+	}
+	
+	public void onRender() {
+		
+		super.onRender();
+	}
+	
+	public void renderPlayer(EntityLivingBase entity){
+		float red = 0f;
+		float green = 1f;
+		float blue = 0f;
+		
+		double xPos = entity.lastTickPosX + (entity.posX - entity.lastTickPosX)*mc.timer.renderPartialTicks;
+	}
+
+}
