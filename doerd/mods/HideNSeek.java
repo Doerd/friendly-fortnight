73beff4102248705e3671c4b6df1fc70e9e995d1
William Adriance <williamadriance@Williams-MacBook-Pro-6.local>
2018-06-01 00:58:19 -0700

    updated

--- doerd/mods/HideNSeek.java
+++ doerd/mods/HideNSeek.java
@@ -1,5 +1,7 @@
 package doerd.mods;
 
+import java.util.ArrayList;
+
 import org.lwjgl.input.Keyboard;
 
 import doerd.main.Category;
@@ -8,20 +10,35 @@ import net.minecraft.client.entity.EntityOtherPlayerMP;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.item.EntityFallingBlock;
+import net.minecraft.util.MovingObjectPosition;
 
 public class HideNSeek extends Module{
 
+	private ArrayList<Integer> players;
+	
 	public HideNSeek() {
 		super("Hide-N-Seek", Keyboard.KEY_Y, Category.RENDER);
-
+		players = new ArrayList<Integer>();
+	}
+	
+	public void onUpdate(){
+		for(Entity e : mc.theWorld.loadedEntityList){
+			if((e instanceof EntityOtherPlayerMP) && (!players.contains(e.getEntityId()))){
+				players.add(e.getEntityId());
+			}
+		}
+		super.onUpdate();
 	}
 	
 	public void onRender() {
 		for(Entity e : mc.theWorld.loadedEntityList){
-			if(e instanceof EntityLiving){
+			if((e instanceof EntityFallingBlock) || 
+					(players.contains(e.getEntityId()) && !(e instanceof EntityOtherPlayerMP))){
 				RenderUtils.entityESPBox(e, 0);
 			}
 		}
+		
 		super.onRender();
 	}
 
